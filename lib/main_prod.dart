132a570426658632176d54840097cc21316b00cf
Hassan Kamal AlMosadder <76610671+Hassank455@users.noreply.github.com>
2025-07-09 15:31:48 +0300

    feat(ios): configure iOS flavors with custom display names and Firebase setup
    - Added GoogleService-Info.plist for each flavor (development, staging,
    production)
    - Implemented Run Script to copy correct plist file during build
    - Defined APP_DISPLAY_NAME per flavor in Build Settings
    - Updated Info.plist to use $(APP_DISPLAY_NAME)
    - Ensured correct display name and Firebase config per environment

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,8 +1,6 @@
 import 'dart:developer';
 
-import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
